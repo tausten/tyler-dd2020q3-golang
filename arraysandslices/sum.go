46b87d7aa2b60f1f57d26e14acccc2e6a8914623
Tyler Austen <tausten@tunein.com>
2020-10-01 18:49:03 +0000

    Wrapped up slices

--- arraysandslices/sum.go
+++ arraysandslices/sum.go
@@ -6,3 +6,21 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numberSlices ...[]int) (sums []int) {
+	for _, numbers := range numberSlices {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numberSlices ...[]int) (sums []int) {
+	for _, numbers := range numberSlices {
+		if len(numbers) < 1 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return
+}
--- arraysandslices/sum_test.go
+++ arraysandslices/sum_test.go
@@ -16,3 +16,30 @@ func TestSumSlice(t *testing.T) {
 	// Then
 	assert.Equal(t, 15, result)
 }
+
+func TestSumAll(t *testing.T) {
+	// Given
+	// When
+	result := SumAll([]int{1, 2}, []int{0, 9})
+
+	// Then
+	assert.ElementsMatch(t, []int{3, 9}, result)
+}
+
+func TestSumAllTails(t *testing.T) {
+	// Given
+	// When
+	result := SumAllTails([]int{1, 2}, []int{0, 9})
+
+	// Then
+	assert.ElementsMatch(t, []int{2, 9}, result)
+}
+
+func TestSumAllTailsEmptySlice(t *testing.T) {
+	// Given
+	// When
+	result := SumAllTails([]int{}, []int{0, 9})
+
+	// Then
+	assert.ElementsMatch(t, []int{0, 9}, result)
+}
