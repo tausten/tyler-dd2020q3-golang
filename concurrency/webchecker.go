b37d6447e211f2487cf8cb716a77718049e30829
Tyler Austen <tausten@tunein.com>
2020-10-08 22:13:29 +0000

    concurrency - initial goroutine + channel implementation

--- concurrency/webchecker.go
+++ concurrency/webchecker.go
@@ -2,11 +2,24 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultsChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultsChannel
+		results[result.string] = result.bool
 	}
 
 	return results
