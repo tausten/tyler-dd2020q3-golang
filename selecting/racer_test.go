f1b22c07165a2cb966c086f0bd3ad77d962c8674
Tyler Austen <tausten@tunein.com>
2020-10-08 22:37:04 +0000

    selecting - some refactoring and introduction of defer

--- selecting/racer_test.go
+++ selecting/racer_test.go
@@ -1,15 +1,24 @@
 package selecting
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRacer(t *testing.T) {
 	// Given
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 	want := fastURL
 
 	// When
@@ -17,4 +26,11 @@ func TestRacer(t *testing.T) {
 
 	// Then
 	assert.Equal(t, want, got)
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
