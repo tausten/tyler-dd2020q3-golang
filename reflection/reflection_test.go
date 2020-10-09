4e05826449ba9ee66bb4f43a98dba274676b646a
Tyler Austen <tausten@tunein.com>
2020-10-09 23:08:28 +0000

    reflection - lots of progress

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,34 +1,108 @@
 package reflection
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-	field := val.Field(0)
+type Person struct {
+	Name    string
+	Profile Profile
+}
 
-	fn(field.String())
+type Profile struct {
+	Age  int
+	City string
 }
 
 func TestWalk(t *testing.T) {
 	// Given
-	const expected = "Chris"
-	var got []string
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non-string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{"Chris", Profile{33, "London"}},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{"Chris", Profile{33, "London"}},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+	}
 
-	x := struct {
-		Name string
-	}{expected}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
 
-	// When
-	walk(x, func(input string) {
-		got = append(got, input)
+			// When
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			// Then
+			assert.Equal(t, test.ExpectedCalls, got)
+		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		// Given
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+		var got []string
+		want := []string{"Bar", "Boz"}
+
+		// When
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		// Then
+		assert.ElementsMatch(t, want, got)
 	})
-
-	// Then
-	assert.Equal(t, 1, len(got))
-	assert.Equal(t, expected, got[0])
 }
