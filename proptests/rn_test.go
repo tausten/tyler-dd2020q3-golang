f7dff61bb30f341071a70714df685876bb87c83f
Tyler Austen <tausten@tunein.com>
2020-10-16 21:44:00 +0000

    proptest - another example of property-based tests in context of roman numerals kata

---
+++ proptests/rn_test.go
@@ -0,0 +1,60 @@
+package proptests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRomanNumerals(t *testing.T) {
+
+	cases := []struct {
+		Arabic int
+		Roman  string
+	}{
+		{Arabic: 1, Roman: "I"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 6, Roman: "VI"},
+		{Arabic: 7, Roman: "VII"},
+		{Arabic: 8, Roman: "VIII"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 10, Roman: "X"},
+		{Arabic: 11, Roman: "XI"},
+		{Arabic: 14, Roman: "XIV"},
+		{Arabic: 15, Roman: "XV"},
+		{Arabic: 18, Roman: "XVIII"},
+		{Arabic: 19, Roman: "XIX"},
+		{Arabic: 20, Roman: "XX"},
+		{Arabic: 39, Roman: "XXXIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 47, Roman: "XLVII"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 50, Roman: "L"},
+		{Arabic: 100, Roman: "C"},
+		{Arabic: 90, Roman: "XC"},
+		{Arabic: 400, Roman: "CD"},
+		{Arabic: 500, Roman: "D"},
+		{Arabic: 900, Roman: "CM"},
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
+		{Arabic: 2014, Roman: "MMXIV"},
+		{Arabic: 1006, Roman: "MVI"},
+		{Arabic: 798, Roman: "DCCXCVIII"},
+	}
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d => %q", test.Arabic, test.Roman), func(t *testing.T) {
+			// Given
+			// When
+			got := ConvertToRoman(test.Arabic)
+
+			// Then
+			assert.Equal(t, test.Roman, got)
+		})
+	}
+}
