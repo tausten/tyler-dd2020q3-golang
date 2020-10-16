7fdc53e4e1d019b7be4000db91bc0be3188df59d
Tyler Austen <tausten@tunein.com>
2020-10-16 18:28:44 +0000

    sync - WaitGroup usage and Mutex

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,7 @@
 package sync
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,7 +11,7 @@ func TestCounter(t *testing.T) {
 
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 		// Given
-		counter := Counter{}
+		counter := NewCounter()
 
 		// When
 		counter.Inc()
@@ -19,5 +20,26 @@ func TestCounter(t *testing.T) {
 
 		// Then
 		assert.Equal(t, 3, counter.Value())
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		// Given
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		// When
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		// Then
+		assert.Equal(t, wantedCount, counter.Value())
 	})
 }
